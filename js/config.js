06187db386f9486dea6924b21d01ce843329a26c
GraceLunaVerdueta <62574500+GraceLunaVerdueta@users.noreply.github.com>
2025-12-04 11:26:33 -0400

    update api_url

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 // js/config.js
 const APP_CONFIG = {
-  csv_url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT28_c_ia7Qb1FKIuQiijNH0p3UnkTuHdy_kdHoYvbg7ihB40HteJ0CgTKEcrnGp8qdGtmi0vUEKUbe/pub?gid=0&single=true&output=csv"
+  csv_url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT28_c_ia7Qb1FKIuQiijNH0p3UnkTuHdy_kdHoYvbg7ihB40HteJ0CgTKEcrnGp8qdGtmi0vUEKUbe/pub?gid=0&single=true&output=csv",
+  api_url: "https://kpi-dashboard-git-main-grace-lunas-projects.vercel.app"
 };
+
